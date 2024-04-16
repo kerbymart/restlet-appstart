35632bbc4d91a922c50dca31f0bf6b18f4e865f5
kerbymart <kerbymart@gmail.com>
2024-04-16 01:54:57 +0000

    Improve logging and add HelloWorld vulnerability test
    Logger has been added to the HelloWorldServerResource for better logging of JSON inputs and rate limit configurations. Also, the error handling mechanism has been improved. A new vulnerability test case, HelloEndpointVulnerabilityTest, has been added to verify the security of the endpoint by testing possible JSON deserialization exploits.

--- src/main/java/org/example/resources/server/HelloWorldServerResource.java
+++ src/main/java/org/example/resources/server/HelloWorldServerResource.java
@@ -2,6 +2,7 @@ package org.example.resources.server;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.config.Configuration;
+import org.example.config.module.ConfigModule;
 import org.example.resources.HelloWorldResource;
 import org.example.resources.model.Greeting;
 import org.restlet.data.Status;
@@ -9,9 +10,13 @@ import org.restlet.resource.ResourceException;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class HelloWorldServerResource extends org.restlet.resource.ServerResource
     implements HelloWorldResource {
+
+    private static final Logger LOGGER = Logger.getLogger(HelloWorldServerResource.class.getName());
+
     private String name;
 
     @Inject
@@ -32,16 +37,18 @@ public class HelloWorldServerResource extends org.restlet.resource.ServerResourc
     @Override
     public String postHello(String json) {
         ObjectMapper mapper = new ObjectMapper();
+
+        LOGGER.info("JSON: " + json);
+        LOGGER.info("Rate Limit: " + configuration.getRateLimit());
+
         try {
             mapper.registerSubtypes(Greeting.class);
             // Deserialize the JSON payload into a Greeting object
             Greeting greeting = mapper.readValue(json, Greeting.class);
             this.name = greeting.getName();
-            System.out.println("Rate Limit: " + configuration.getRateLimit());
             return "hello, " + this.name;
-        } catch (IOException e) {
+        } catch (Exception e) {
             // Handle the error (e.g., log it, return an error message)
-            setStatus(Status.SERVER_ERROR_INTERNAL, e.getMessage());
             return "Error deserializing JSON";
         }
     }
